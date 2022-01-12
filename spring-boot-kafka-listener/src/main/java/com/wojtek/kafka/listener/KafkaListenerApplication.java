7564e248a6729a5ce4b473fa5604e396158a42ff
wmaliga <wojciech.maliga@gmail.com>
2022-01-12 21:53:57 +0100

    Spring Boot Application

---
+++ spring-boot-kafka-listener/src/main/java/com/wojtek/kafka/listener/KafkaListenerApplication.java
@@ -0,0 +1,12 @@
+package com.wojtek.kafka.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaListenerApplication.class, args);
+    }
+}
